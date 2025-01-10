1707ab9f248faeae4fa816b3b3f5b102b71a27d7
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-01-10 23:34:52 +0530

    navbar routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center p-5'>
-      <div className='text-white'>TDS LOGO</div>
+    <div className='flex justify-between items-center p-5 backdrop-filter backdrop-blur-lg'>
+      <Link to='/'>
+      {/* to be replace with img tag */}
+        <div className='text-white'>TDS LOGO</div>
+      </Link>
       <div className='text-white flex gap-5'>
-        <Link>Home</Link>
-        <Link>About</Link>
-        <Link>Contact</Link>
-        <Link>Alumni</Link>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/alumni'>Alumni</Link>
       </div>
     </div>
   )
