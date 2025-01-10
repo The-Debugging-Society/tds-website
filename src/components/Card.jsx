82a569eb59ada42d3e95d1a02f205fdb4ad6c07a
sujal <me@sujal.info>
2025-01-10 21:12:43 +0530

    magic ui setup

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import { MagicCard } from "./ui/magic-card";
+
+export function MagicCardDemo() {
+  const { theme } = "dark";
+  return (
+    <div
+      className={
+        "flex h-[500px] w-1/3 flex-col gap-4 lg:h-[250px] lg:flex-row"
+      }
+    >
+      <MagicCard
+        className="cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl"
+        gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+      >
+        Magic
+      </MagicCard>
+      <MagicCard
+        className="cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl"
+        gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+      >
+        Card
+      </MagicCard>
+    </div>
+  );
+}
