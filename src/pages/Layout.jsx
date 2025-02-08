be8d6b867ba938b6f7ae9a2973a38a9ec722f56a
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-08 17:17:06 +0530

    style scrollbar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import {Navbar,Footer} from '../components/index.js'
+import { Navbar, Footer } from '../components/index.js'
 import { Outlet } from 'react-router-dom'
 import { Loader } from '../components/Loader.jsx'
 import { useState, useEffect } from 'react'
+import { ScrollArea } from '@/components/ui/scroll-area.jsx'
 
 function Layout() {
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }, []);
-    if (isLoading) {
-      return <Loader />;
-    }
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <ScrollArea className="h-screen w-full">
+      <Navbar />
+      <Outlet />
+      <Footer />
+      </ScrollArea>
     </>
   )
 }
