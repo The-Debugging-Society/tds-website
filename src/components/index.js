411fff4ca7bf1483eb329eef311a1bc4f15b4be3
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-14 22:22:06 +0530

    data management and alumni responsive

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import Hero from "./Home/Hero";
 import Navbar from "./Header/Navbar";
 import MarqueeDemo from "./Home/Review";
 import PastEvents from "./Events/EventsNavigation";
-import { MeetTDSCard } from "./About/MeetTDSCard";
+import { MeetTDSCard } from "./About/TeamCard";
 import Events from "./Events/Events";
 
 export { AboutSection, MagicCardDemo, VelocityScrollText ,BentoDemo, Footer, Hero, Navbar, MarqueeDemo, PastEvents,MeetTDSCard,Events };  
