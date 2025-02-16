274aa26340f03c9d2961a4206ba052781ed9caef
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-17 01:23:52 +0530

    about page

--- src/components/About/TeamCard.jsx
+++ src/components/About/TeamCard.jsx
@@ -1,6 +1,6 @@
 import { IconBrandLinkedin } from '@tabler/icons-react'
 
-export const MeetTDSCard = ({
+export const TeamCard = ({
   photoUrl,
   name,
   position,
@@ -9,13 +9,11 @@ export const MeetTDSCard = ({
 }) => {
   return (
     <div className=" rounded-lg p-5 overflow-hidden shadow-lg bg-[#0a0a0a] text-white border border-[#1f1f1f] transition-all duration-300 ease-in-out hover:shadow-xl hover:scale-105">
-      <div className="overflow-hidden">
-        <img
-          className="w-full rounded-lg h-64 object-cover object-center transition-transform duration-300 ease-in-out hover:scale-110"
-          src={photoUrl}
-          alt={`${name}'s profile`}
-        />
-      </div>
+      <img
+        className="w-full rounded-lg h-48 md:h-64 object-cover object-center"
+        src={photoUrl}
+        alt={`${name}'s profile`}
+      />
       <div className="px-2 py-4">
         <div className="flex items-center gap-2">
           <h3 className="font-bold text-2xl mb-1 text-white">{name}</h3>
