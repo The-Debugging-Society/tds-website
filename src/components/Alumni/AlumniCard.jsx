47adcebb719eef472656c90b78ccc2d72fa58cf3
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-17 01:20:06 +0530

    alumni page

--- src/components/Alumni/AlumniCard.jsx
+++ src/components/Alumni/AlumniCard.jsx
@@ -1,29 +1,33 @@
 import { IconBrandLinkedin } from '@tabler/icons-react'
 
-const AlumniCard = ({ name, passoutYear, company, linkedin }) => {
+const AlumniCard = ({ name, image, por, company, linkedin }) => {
+  const displayName = name.split(' ').slice(0, 2).join(' ')
+
   return (
-    <div className="max-w-md flex flex-col md:flex-row justify-between items-center bg-gray-700/50 rounded-lg px-8 py-10 hover:scale-105 gap-10">
-      <div className="flex items-center mb-4 md:mb-0">
+    <div className="max-w-md flex flex-col justify-between items-center bg-gray-700/50 rounded-lg px-8 py-10 hover:scale-105 gap-6">
+      <div className="flex items-center">
         <img
-          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-          className="h-36 w-36 object-cover rounded-full"
+          src={image}
+          alt={name}
+          className="h-28 w-28 object-cover rounded-full"
         />
       </div>
       <div className="text-center md:text-left">
-        <h2 className="text-xl font-bold mb-2 text-white">{name}</h2>
-        {passoutYear && <p 
-        className="text-gray-300 mb-1">Passout Year: {passoutYear}
-        </p>}
+        <h2 className="text-xl font-bold mb-2 text-white flex items-center gap-2">{displayName} {linkedin && (
+          <a
+            href={linkedin}
+            className="text-blue-500 hover:underline flex items-center justify-center md:justify-start"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconBrandLinkedin className="mr-2 h-6 w-6" />
+          </a>
+        )}</h2>
+        {por && (
+          <p className="text-gray-300 mb-1">{por}</p>
+        )}
         <p className="text-gray-400 mb-4">Company: {company}</p>
-        {linkedin && <a
-          href={linkedin}
-          className="text-blue-500 hover:underline flex items-center justify-center md:justify-start"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <IconBrandLinkedin className="mr-2 h-6 w-6" />
-        </a>}
+        
       </div>
     </div>
   )
