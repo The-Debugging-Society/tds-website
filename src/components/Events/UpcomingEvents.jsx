585c554015cc438fbacc3e67d03422572044d7d9
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-03-17 20:57:34 +0530

    feat: implement UpcomingEvents component with no events message

--- src/components/Events/UpcomingEvents.jsx
+++ src/components/Events/UpcomingEvents.jsx
@@ -1,32 +1,12 @@
 import { EventCard } from "./UpcomingEventCard";
 import React from "react";
+import { CalendarPlus } from "lucide-react";
 
-function PastEvents() {
-    const events = [
-        {
-            title: "CTRL+ALT+HACK",
-            description: "The SpaceCon Hackathon is a 24-hour innovation marathon where brilliant minds collaborate to ideate, design, and prototype solutions using Generative AI, Machine Learning, and Web 3.0/Blockchain.",
-            image: "https://spacecon.tech/ctrlalthack.png"
-        },
-        {
-            title: "DATA SPHERE",
-            description: "This is the data analytics event of SpaceCon. As visualization takes centre stage, the event is all about drawing insights from the given data. It is the ultimate test of your analytical skills and storytelling as it involves presenting your insights in a creative and impactful way.",
-            image: "https://spacecon.tech/Datasphereweb.png"
-        },
-        {
-            title: "WEB-A-THON",
-            description: "Web-a-Thon is a frontend-focused website design challenge where participants will craft visually stunning and innovative websites based on space-related problem statements. Entries will be judged on design, creativity, responsiveness, and code structure.",
-            image: "https://spacecon.tech/webathon.png"
-        },
-        {
-            title: "AI HORIZON",
-            description: "Generative AI Challenge blends tech and creativity to explore AI's role in space exploration and storytelling. Focus on AI-driven research, planning, and missions. Workshops on AI innovations shaping human space journeys. If you're into space, AI, or creative tech, explore the future of space exploration!",
-            image: "https://spacecon.tech/aihorizon.png"
-        }
-    ];
+function UpcomingEvent() {
+    const events = [];
 
     return (
-        <div
+        events.length > 0 ? <div
             className="flex rounded-3xl animate-on-scroll m-auto flex-col justify-center items-center gap-5 pt-0 text-center"
             style={{
                 boxShadow: "0px 0px 14px 1px #5e5e5e inset",
@@ -35,21 +15,40 @@ function PastEvents() {
                 maxWidth: "1320px",
                 margin: "40px auto",
                 backgroundColor: "#121212"
-            }}>
+            }}
+        >
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto">
                 {events.map((card, index) => (
-                    <div key={index} className="w-full flex">
-                        <EventCard
-                            title={card.title}
-                            description={card.description}
-                            image={card.image}
-                            style={{ height: "100%" }}
-                        />
+                        <div key={index} className="w-full flex">
+                            <EventCard
+                                title={card.title}
+                                description={card.description}
+                                image={card.image}
+                                style={{ height: "100%" }}
+                            />
                     </div>
                 ))}
             </div>
+        </div> : 
+        <div className="rounded-3xl animate-on-scroll m-auto" style={{
+            boxShadow: "0px 0px 14px 1px #5e5e5e inset",
+            boxSizing: "border-box",
+            padding: "50px",
+            maxWidth: "1320px",
+            margin: "40px auto",
+            backgroundColor: "#121212"
+        }}>
+            <div className="col-span-full flex min-h-[400px] flex-col items-center justify-center rounded-lg border border-dashed border-gray-700 p-8 text-center animate-in fade-in-50 w-full mx-auto">
+            <CalendarPlus className="mr-2 h-24 w-24" />
+          <h3 className="mt-6 text-2xl font-semibold text-white">No upcoming events</h3>
+          <p className="mt-2 mb-8 max-w-sm text-center text-gray-400">
+            There are no events scheduled at the moment.
+            <br />
+            Check back later.
+          </p>
+        </div>
         </div>
     );
 }
 
-export default PastEvents;
+export default UpcomingEvent;
